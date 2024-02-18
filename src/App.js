72934beebea96c80247ad857f978bf57721584b8
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-18 15:52:33 +0300

    feat: Implement addNinja function to dynamically generate unique IDs for ninjas and update state with the new ninja. Also, add logging for the newly added ninja.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component} from "react";
 import Ninja from "./Ninja";
 import Plumbers from "./Plumbers";
 import AddNinja from "./AddNinja";
-import AddPlumber from "./AddPlumber";
 
 class App extends Component {
   state ={
@@ -12,15 +11,24 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+    console.log(ninja);
+  }
+
+  
   render() {
     return (
       <div>
         <h1>AM BEK!!!!!!</h1>
         <h3>Short Intro:</h3>
         < Ninja ninjas = {this.state.ninjas} />
-        < AddNinja />
+        < AddNinja addNinja={this.addNinja} />
         < Plumbers />
-        < AddPlumber />
       </div>
     );
   }
