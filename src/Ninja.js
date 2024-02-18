3fa62f702d34256e99182ed3e0c95f4b224c766e
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-18 03:36:29 +0300

    feat: Create Ninja component with props

--- src/Ninja.js
+++ src/Ninja.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class Ninja extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="ninja">
-        <div>Name: Sam Amigo</div>
-        <div>Age: 30</div>
-        <div>Belt: Black</div>
+        <div>Name: {this.props.name}</div>
+        <div>Age: {this.props.age}</div>
+        <div>Belt: {this.props.belt}</div>
       </div>
     );
   }
