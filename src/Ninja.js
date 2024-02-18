907cb468a1a55202ae3c104555ced2ae83b625d1
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-18 03:15:00 +0300

    Create new component and nest it inside the App.js

---
+++ src/Ninja.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Ninja extends Component {
+  render() {
+    return (
+      <div className="ninja">
+        <div>Name: Sam Amigo</div>
+        <div>Age: 30</div>
+        <div>Belt: Black</div>
+      </div>
+    );
+  }
+}
+
+export default Ninja;
