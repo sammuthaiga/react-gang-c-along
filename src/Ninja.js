b77f18f10f69beee6678f21e0b669146a58891bf
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-18 14:44:25 +0300

    feat: Use if statement to conditionally render ninjas only if they are above 20 years old

--- src/Ninja.js
+++ src/Ninja.js
@@ -4,13 +4,19 @@ const Ninja = ({ninjas}) => {
   
     console.log({ninjas});
     const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
+      // conditional Rendering.
+      if (ninja.age > 20) {
+        return (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+          </div>
+        );
+      } else {
+        return null;
+      }
+      
     });
     return (
       <div className="ninja-list">
