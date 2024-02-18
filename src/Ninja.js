1beff582d30fce8e15a14fca931ef7aca5a9c9c9
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-18 14:51:14 +0300

    refactor: use the ternary operator to conditionally render ninjas based on if age is is greater than 20

--- src/Ninja.js
+++ src/Ninja.js
@@ -3,21 +3,32 @@ import React from "react";
 const Ninja = ({ninjas}) => {
   
     console.log({ninjas});
-    const ninjaList = ninjas.map(ninja => {
-      // conditional Rendering.
-      if (ninja.age > 20) {
-        return (
-          <div className="ninja" key={ninja.id}>
-            <div>Name: {ninja.name}</div>
-            <div>Age: {ninja.age}</div>
-            <div>Belt: {ninja.belt}</div>
-          </div>
-        );
-      } else {
-        return null;
-      }
+    // const ninjaList = ninjas.map(ninja => {
+      // conditional Rendering using the if statement
+      // if (ninja.age > 20) {
+      //   return (
+      //     <div className="ninja" key={ninja.id}>
+      //       <div>Name: {ninja.name}</div>
+      //       <div>Age: {ninja.age}</div>
+      //       <div>Belt: {ninja.belt}</div>
+      //     </div>
+      //   );
+      // } else {
+      //   return null;
+      // }
       
-    });
+    // });
+    // conditional rendering using the ternary operator
+    const ninjaList = ninjas.map(ninja => {
+      return ninja.age > 20 ? (
+        <div className="ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+        </div>
+      ) : null;
+    
+    })
     return (
       <div className="ninja-list">
         { ninjaList }
